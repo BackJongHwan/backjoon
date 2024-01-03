49e10856d80e0b2245c933284f1307827b498151
back <bjh59460@gmail.com>
2024-01-03 16:01:43 +0900

    step6 complete

--- c++/step6/grade.cpp
+++ c++/step6/grade.cpp
@@ -1,26 +1,74 @@
-#include<iostream>
-#include<string>
-class Subject{
-    private:
-    std::string subject;
-    float credit;
-    std::string grade;
-}
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// #define num 20
+
+// class Subject{
+//     public:
+//         string subject;
+//         float credit;
+//         string grade;
+//         void print(){
+//             cout<< this->subject;
+//             cout<< this->credit;
+//             cout<< this->grade;
+//         }
+// };
 
 
-int main()
-{
-    class Sub[20];
-    //input the grade
-    for(int i = 0; i<20; i++)
-    {
-        std::cin >> Sub[i].subject;
-        std::cin >> Sub[i].credit;
-        std::cin >> Sub[i].grade;
-    }
-    C:\Users\wantg\Downloads\vimrc2_lsp
+// // 
+// int main()
+// {
+//     Subject sub[num];
+//     for(int i = 0; i < num; i++)
+//     {
+//         cin >> sub[i].subject;
+//         cin >> sub[i].credit;
+//         cin >> sub[i].grade;
+//     }
 
-    
+//     float total_credit = 0;
+//     float total_grade = 0;
+
+//     for(int i =  0; i < num; i++)
+//     {
+//         if(sub[i].grade[0] == 'P')
+//             continue;
+//         if(sub[i].grade[0] == 'F'){
+//             total_credit += sub[i].credit;
+//             continue;
+//         }
+
+//         if(sub[i].grade[1] == '+')
+//         {
+//             total_grade += sub[i].credit * 0.5;
+//         }
+
+//         if(sub[i].grade[0] == 'A'){
+//             total_credit += sub[i].credit;
+//             total_grade += sub[i].credit * 4.0;
+//         }
+//         else if(sub[i].grade[0] == 'B')
+//         {
+//             total_credit += sub[i].credit;
+//             total_grade += sub[i].credit* 3.0;
+//         }
+//         else if(sub[i].grade[0] == 'C')
+//         {
+//             total_credit += sub[i].credit;
+//             total_grade += sub[i].credit* 2.0;
+
+//         }
+//         else if(sub[i].grade[0] == 'D')
+//         {
+//             total_credit += sub[i].credit;
+//             total_grade += sub[i].credit* 1.0;
+
+//         }
+//     }
 
 
-}
+//     cout<< total_grade / total_credit << endl;
+//     return 0;
+
+// }
---
+++ c++/step7/matrix.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+int main()
+{
+    int m, n;
+    std::cin >> m >> n;
+    int **a, **b = new int*[m];
+
+    for(int i = 0; i < m; i++)
+    {
+        a[i] = new int[n];
+        b[i] = new int[n];
+    }
+    
+
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            std::cin>> a[i][j];
+        }
+    }
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            std::cin>> b[i][j];
+            a[i][j] += b[i][j];
+        }
+    }
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            std::cout<<a[i][j]<<" ";
+        }
+        std::cout<< '\n';
+    }
+
+
+}
