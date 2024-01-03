8c1c921ee4f4097eed27876450a3c52793510879
back <bjh59460@gmail.com>
2024-01-03 22:22:35 +0900

    step7 complete!

--- c++/step7/vertical.cpp
+++ c++/step7/vertical.cpp
@@ -1,26 +1,19 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-    char toy [5][15];
-    string str ="";
-    char temp;
-
+    char toy[5][15];
     for(int i = 0; i < 5; i++)
-    {
+        for(int j = 0; j < 15; j++)
+            toy[i][j] = '\n';
+    for(int i = 0; i < 5; i++)
         cin >> toy[i];
-    }
-
-    for(int j = 0; j < 15; j++)
-    {
-        if(toy[0][j] == NULL)
-            break;
-        for(int i =0; i < 5; i++)
-        {
-            if(toy[i][j] != ' ')
-                str += toy[i][j];
-        }
-    }
-    cout<<str<<endl;
+    for(int j =0 ; j < 15; j++)
+        for(int i = 0; i < 5; i++)
+            if(('A'<=toy[i][j] && toy[i][j] <= 'Z')||('a'<=toy[i][j] && toy[i][j]<= 'z')||('0'<=toy[i][j] && toy[i][j] <= '9'))
+                {
+                    cout<<toy[i][j];
+                }
 
 }
