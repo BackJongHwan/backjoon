495ddddef8e89b6a0b11696a3f44a8e2ad85a24d
back <bjh59460@gmail.com>
2024-06-30 22:27:16 +0900

    doing chap

--- python/temp.py
+++ python/temp.py
@@ -1,6 +1,16 @@
-n = int(input())
-score = list(map(int, input().split()))
-max_score = max(score)
-new_score = list(map(lambda x: x / max_score * 100, score))
-average = sum(new_score) / len(new_score)
-print(average)
+result = '?'
+str = input()
+max = [[], 0]
+for i in range(len(str)):
+    char = str[i].upper()
+    if(str[i] in max[0]):
+        continue
+    else:
+        frq = str.count(str[i])
+        if(max[1] < frq):
+            max[0].append(str[i])
+            max[1] = frq
+            result = str[i]
+        elif(max[1] == frq):
+            result = '?'
+print(result)
