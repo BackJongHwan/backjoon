735a386bcf3769181eb979f510e09dca1342b949
Mr.back <bjh59460@gmail.com>
2024-12-27 16:46:05 +0900

    1629..

--- class/class4/unsolved/1629.py
+++ class/class4/unsolved/1629.py
@@ -1,5 +1,48 @@
+# #
+# # a = 5
+# # b = 7
+# # for i in range(10):
+# #     print(a % b, i + 1, a)
+# #     a *= a
+# # 반복되는 특성을 가지는데 
+# a, b, c= map(int, input().split())
+# # check = False
+# dict = {}
+# dict2 = {}
+# # if c is 1 나머지는 0
+# if(c == 1):
+#     print(0)
+# else:
+#     # a가 1이면 c가 0이 아닌 이상 나머지는 1
+#     if(a == 1):
+#         print(1)
+#     else:
+#         #general case
+#         idx = 1
+#         num = a
+#         while(True):
+#             temp = num % c
+#             num = (a * temp) % c
+#             if temp in dict:
+#                 break
+#             else:
+#                 dict[temp] = idx
+#                 dict2[idx] = temp
+#                 idx +=1
+#         idx -= 1
+#         # print(dict)
+#         # print(idx, b)
+#         result = b % idx 
+#         # print(result)
+#         # print(dict)
+#         # print(dict2)
+#         print(dict2[result])
+
+'''
+나머지 법칙과 지수법칙을 이용하여 해결
+지수법칙: A^m +n = A^m * A^n
+나머지 법칙 = (AB) % c = (A % C)(B % C)
+'''
+def divide(a, b):
+    if(b == 1)
 a, b, c= map(int, input().split())
-if(a == 1):
-    print(1)
-else:
-    
