090bdd0e4efae23a8aea41e80438a5883003b4bf
back <bjh59460@gmail.com>
2024-12-31 03:57:50 +0900

    17070

---
+++ class/class4/unsolved/17070.py
@@ -0,0 +1,11 @@
+n = int(input())
+matrix = []
+for _ in range(n):
+    temp = list(map(int, input().split()))
+    matrix.append(temp)
+# visited = [[False] * n for _ in range(n)]
+
+
+
+result = 0
+print(result)
