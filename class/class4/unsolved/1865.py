8bc4b544208a48c891e86a9cb6ded2a826bc36c6
Mr.back <bjh59460@gmail.com>
2025-01-08 16:11:30 +0900

    1/8 end

--- class/class4/unsolved/1865.py
+++
@@ -1,6 +0,0 @@
-# floyd 사용해서 그냥 하려고 했는데 O(N^3)으로 안 됨
-# 인접리스트로 하는 것이 일반적이고 인접행렬을 한다고 달라지지 않음
-
-import sys
-input = sys.stdin.readline
-# graph 입력받기 -> adjecent list 이용 -> 이것도 플로이드 알고리즘을 이용한다면 O(N^3)
