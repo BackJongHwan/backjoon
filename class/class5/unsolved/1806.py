566b51fd8e2629563498054d6a7276bc1ef24053
back <bjh59460@gmail.com>
2025-02-07 13:53:26 +0900

    1806

--- class/class5/unsolved/1806.py
+++ class/class5/unsolved/1806.py
@@ -5,11 +5,29 @@ input = sys.stdin.readline
 n, s = map(int ,input().split())
 nums = list(map(int , input().split()))
 # print(nums)
-sums = [0] * (n + 1)
-sums[1] = nums[0]
-for i in range(n):
-    sums[i + 1] = sums[i] + nums[i]
-if sums[n] < s:
-    print(0)
-else:
+# 이 방식은 left, right의 크기가 sort되어 있지 않다면 맞지 않는 방식!!
+def two_pointer_wrong():
+    # 초기 상태: 전체 배열 길이로 시작
+    left, right = 0, n
+    current_sum = sum(nums)  # 처음에는 전체 합으로 시작
+    if current_sum < s:
+        return 0
+    min_length = n  # 최소 길이 설정
+    while left <= right:
+        if current_sum >= s:
+            min_length = min(min_length, right - left)
+            if nums[left] < nums[right -1]:
+                current_sum -= nums[left]
+                left += 1
+            else:
+                current_sum -= nums[right - 1]
+                right -= 1
+        else:
+            break
+    return min_length
+def two_pointer():
+    left, right = 0, 0
+    current_sum = 0
     
+result = two_pointer()
+print(result)
